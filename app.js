13daa8bdc26ddeada9f4e86bdc8b86f7c4907dbe
Chaitanya Kumar <chaitanya4real@gmail.com>
2021-07-12 04:26:12 +0530

    3d effects added

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const purchase = document.querySelector('.purchase button');
+const sizes = document.querySelector('.sizes');
+const info = document.querySelector('.info h5');
 
 //movement animation event
 container.addEventListener('mousemove', (e)=>{
@@ -10,9 +15,21 @@ container.addEventListener('mousemove', (e)=>{
 //animate in
 container.addEventListener('mouseenter', (e) =>{
     card.style.transition = 'none';
+    //popout
+    title.style.transform = 'translateZ(150px)';
+    sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)';
+    info.style.transform = 'translateZ(125px)';
+    sizes.style.transform = 'translateZ(100px)';
+    purchase.style.transform = 'translateZ(75px)';
 })
 //amimate out
 container.addEventListener('mouseleave', (e) =>{
     card.style.transition= 'all 0.5s ease';
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    //popback
+    title.style.transform = 'translateZ(0px)';
+    sneaker.style.transform = 'translateZ(0px) rotateZ(0deg)';
+    info.style.transform = 'translateZ(0px)';
+    sizes.style.transform = 'translateZ(0px)';
+    purchase.style.transform = 'translateZ(0px)';
 });
